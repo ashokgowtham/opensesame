2eec1c44e2243a26b9c483c6bb70236554b29adc
Simon <simonroy@thoughtworks.com>
2014-08-06 22:14:30 +0530

    If sidebar is not visible do not toggle visibility.

--- opensesame.py
+++ opensesame.py
@@ -1,6 +1,13 @@
 import sublime, sublime_plugin
-
+sidebarVisible = True
 class SideBarNavigator(sublime_plugin.EventListener):
     def on_activated(self, view):
         window = sublime.active_window()
-        window.run_command('reveal_in_side_bar')
+        global sidebarVisible
+        if sidebarVisible:
+            window.run_command('reveal_in_side_bar')
+
+    def on_window_command(self, window, command_name, args):
+        if command_name == "toggle_side_bar":
+            global sidebarVisible
+            sidebarVisible = not sidebarVisible
